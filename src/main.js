f3c0ca56b862bf4bd2fdd974a4261a0d6b1b8549
cholen <cholen@foxmail.com>
2021-12-03 20:55:26 +0800

    添加图片处理

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import ('./main.css')
+require('./main.css')
+
+//加载logo.gif文件
+const logo = require('./logo.gif')
 
 document.querySelector('#btn').addEventListener('click', function () {
     alert('You clicked me!');
- },false)
+}, false)
+ 
+// 获取图片元素节点，手动设置图片链接
+document.querySelector('#logo').src = logo;
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,21 @@ module.exports = {
                 // css-loader完成CSS文件到JavaScript模块的转换
                 // style-loader将JavaScript模块中的样式内容通过DOM操作写入到HTML页面的style节点中
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                // 以gif/png/jpg结尾的文件应用file-loader
+                test: /\.(gif|png|jpg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // 构建文件名和目录设置，[name]和[ext]是占位符
+                            name: 'images/[name].[ext]',
+                            esModule: false, //是否启用ES6模块系统
+                            publicPath: 'build', //输出的根目录
+                        }
+                    }
+                ]
             }
         ]
     }
