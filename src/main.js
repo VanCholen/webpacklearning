7ca30d7baf318d26a0d6f849a16744e37b1001b3
cholen <cholen@foxmail.com>
2021-12-03 20:39:01 +0800

    添加main.css并增加CSS处理

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import ('./main.css')
+
 document.querySelector('#btn').addEventListener('click', function () {
     alert('You clicked me!');
  },false)
--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
     },
     //构建方式设置为开发模式
     mode: 'development',
+    
+    // 一些模块 譬如css-loader、style-loader等的配置
+    module: {
+        rules: [
+            {
+                // 以css结尾的文件应用下面的loader
+                test: /\.css$/,
+                // 顺序从右往左依次使用css-loader、再使用style-loader
+                // css-loader完成CSS文件到JavaScript模块的转换
+                // style-loader将JavaScript模块中的样式内容通过DOM操作写入到HTML页面的style节点中
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
